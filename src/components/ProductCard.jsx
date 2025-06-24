05963399b4ba0723181f1fbb0a1e22089820a44b
linoSAP <157139505+linoSAP@users.noreply.github.com>
2025-06-24 19:39:19 +0100

    Update ProductCard.jsx
    pannier sur add

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -69,12 +69,15 @@ export default function ProductCard({ product, onAddToCart, isFeatured = false, 
             viewBox="0 0 24 24"
             stroke="currentColor"
           >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth={2}
-              d={isFeatured ? "M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" : "M12 6v6m0 0v6m0-6h6m-6 0H6"}
-            />
+           <path
+    strokeLinecap="round"
+    strokeLinejoin="round"
+    strokeWidth={2}
+    d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13l-2.293 
+    2.293c-.63.63-.184 1.707.707 1.707H17m0 
+    0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 
+    11-4 0 2 2 0 014 0z"
+  />
           </svg>
           {isFeatured ? 'Découvrir' : 'Ajouter'}
         </button>
